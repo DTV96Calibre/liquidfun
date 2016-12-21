1c90f1360b7f1527868eb52c2b635f11036899b5
Cameron Martens <camerondm9@yahoo.ca>
2016-12-20 21:11:52 -0600

    Fixed js b2AABB.GetCenter

--- liquidfun/Box2D/lfjs/jsBindings/Collision/b2Collision.js
+++ liquidfun/Box2D/lfjs/jsBindings/Collision/b2Collision.js
@@ -8,6 +8,7 @@ b2AABB.prototype.GetCenter = function() {
   var sum = new b2Vec2();
   b2Vec2.Add(sum, this.lowerBound, this.upperBound);
   b2Vec2.MulScalar(sum, sum, 0.5);
+  return sum;
 };
 
 // todo use just the pointer and offsets to get this data directly from the heap
